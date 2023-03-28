e33eb416b785839317210195aee1ea97cfd5be2c
vibhutisharma1 <sharma.vibhuti@utexas.edu>
2023-03-28 17:26:24 -0500

    done w journal screen

---
+++ leaf-journal/PhotoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  leaf-journal
+//
+//  Created by Vibhuti Sharma on 3/25/23.
+//
+
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
