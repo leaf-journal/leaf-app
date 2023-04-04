6c252c80ab53b25f2ed78c7cf1145ea9134cc635
vibhutisharma1 <sharma.vibhuti@utexas.edu>
2023-04-04 15:45:19 -0500

    merged all screens

--- leaf-journal/PhotoViewController.swift
+++ leaf-journal/PhotoViewController.swift
@@ -8,10 +8,51 @@
 
 import UIKit
 
-class PhotoViewController: UIViewController {
+
+
+class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var captionBackground: UILabel!
     
+    @IBAction func uploadButtonTapped(_ sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = pickedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // get the current date
+        let currentDate = Date()
+        
+        // create a date formatter
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yy"
+        
+        // set the formatted date as the text of the label
+        dateLabel.text = dateFormatter.string(from: currentDate)
+        
+        addButton.layer.cornerRadius = 20
+        addButton.layer.masksToBounds = true
+        
+        
+        
+        captionBackground.layer.cornerRadius = 10
+        captionBackground.layer.masksToBounds = true
+        
+      
     }
 }
