f50c0c8c2b682aaf44c4a4a6eab8288cf05479e5
faizarahman <faiza2002@gmail.com>
2023-04-04 19:07:26 -0500

    created uikit viewcontrollers for home and profile

--- leaf-journal/HomeViewController.swift
+++ leaf-journal/HomeViewController.swift
@@ -15,4 +15,20 @@ class HomeViewController: UIViewController {
     }
     @IBAction func MemoriesButton(_ sender: UIButton) {
     }
+    
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            // Connect the PressMemories button to the action method
+            MemoriesButton.addTarget(self, action: #selector(pressMemoriesButtonClicked(_:)), for: .touchUpInside)
+    }
+        
+    // Action method for PressMemories button click
+    @IBAction func pressMemoriesButtonClicked(_ sender: UIButton) {
+        // Instantiate the Profile view controller
+        let profileVC = ProfileViewController()
+            
+        // Present the Profile view controller modally
+        self.present(profileVC, animated: true, completion: nil)
+    }
 }
--- leaf-journal/ProfileViewController.swift
+++ leaf-journal/ProfileViewController.swift
@@ -5,16 +5,7 @@
 //  Created by Faiza Rahman on 4/4/23.
 //
 
-import SwiftUI
+import UIKit
 
-struct ProfileViewController: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct ProfileViewController_Previews: PreviewProvider {
-    static var previews: some View {
-        ProfileViewController()
-    }
+class ProfileViewController: UIViewController {
 }
