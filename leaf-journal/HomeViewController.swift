5b1357b5ea4c50dbc033da6265443a43f850ec57
faizarahman <faiza2002@gmail.com>
2023-04-04 19:09:19 -0500

    updated view controllers

--- leaf-journal/HomeViewController.swift
+++ leaf-journal/HomeViewController.swift
@@ -21,6 +21,8 @@ class HomeViewController: UIViewController {
             
             // Connect the PressMemories button to the action method
             MemoriesButton.addTarget(self, action: #selector(pressMemoriesButtonClicked(_:)), for: .touchUpInside)
+        
+            JournalButton.addTarget(self, action: #selector(pressJournalButtonClicked(_:)), for: .touchUpInside)
     }
         
     // Action method for PressMemories button click
@@ -30,5 +32,13 @@ class HomeViewController: UIViewController {
             
         // Present the Profile view controller modally
         self.present(profileVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func pressJournalButtonClicked(_ sender: UIButton) {
+        // Instantiate the Profile view controller
+        let journalVC = ViewController()
+            
+        // Present the Profile view controller modally
+        self.present(journalVC, animated: true, completion: nil)
     }
 }
