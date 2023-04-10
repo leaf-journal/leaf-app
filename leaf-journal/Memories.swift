3cda291b2dc5be4ec56ca80c34397d18784de0f5
Linh <linhvu.138@utexas.edu>
2023-04-09 21:44:53 -0500

    working table view

--- leaf-journal/Memories.swift
+++ leaf-journal/Memories.swift
@@ -8,10 +8,39 @@
 import Foundation
 import UIKit
 
-class Memories: UIViewController {
+class Memories: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
     @IBOutlet weak var table: UITableView!
+    
+    struct Pictures {
+        let title: String
+        let imageName: String
+    }
+    
+    let pictures = [
+        Pictures(title: "Cute cat", imageName: "image")
+        
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        table.dataSource = self
+        table.delegate = self
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let picture = pictures[indexPath.row]
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MemoriesTableViewCell
+        cell.lable.text = picture.title
+        cell.iconImageView.image = UIImage(named: picture.imageName)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
     }
 }
---
+++ leaf-journal/MemoriesTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MemoriesTableViewCell.swift
+//  leaf-journal
+//
+//  Created by L V on 4/9/23.
+//
+
+import UIKit
+
+class MemoriesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var lable: UILabel!
+
+}
