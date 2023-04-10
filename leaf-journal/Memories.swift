9192eaf53097d3e5c68fc54d8a27bb8684b97c7b
Linh <linhvu.138@utexas.edu>
2023-04-09 19:35:51 -0500

    created table view for memories

---
+++ leaf-journal/Memories.swift
@@ -0,0 +1,17 @@
+//
+//  Memories.swift
+//  leaf-journal
+//
+//  Created by L V on 4/9/23.
+//
+
+import Foundation
+import UIKit
+
+class Memories: UIViewController {
+    @IBOutlet weak var table: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
