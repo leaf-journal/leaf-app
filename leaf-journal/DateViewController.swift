05d468ed53ef550ead6325f38255817e3a02fb53
vibhutisharma1 <sharma.vibhuti@utexas.edu>
2023-04-11 12:06:49 -0500

    realm updates in real time for memory page

--- leaf-journal/DateViewController.swift
+++ leaf-journal/DateViewController.swift
@@ -13,6 +13,8 @@ class DateViewController: UIViewController {
     var selectedDate: String?
     
     let realm = try! Realm()
+    
+    var notificationToken: NotificationToken? // add this property
 
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var journalEntry: UITextView!
@@ -21,6 +23,7 @@ class DateViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
         let formatDate = DateFormatter()
         formatDate.dateFormat = "MM-dd-yy"
         let foundationDate = Foundation.Date()
@@ -32,7 +35,7 @@ class DateViewController: UIViewController {
         let entry = realm.objects(Entry.self).filter("dayCurrent = %@", specificDate).first
         journalEntry.text = entry?.journal
         photoCaption.text = entry?.photoCaption
-        
+
         // Assuming entry is an instance of the Entry object
         let imagePath = specificDate + ".jpg"
         let fileManager = FileManager.default
@@ -48,8 +51,40 @@ class DateViewController: UIViewController {
                 print("Error loading image: \(error.localizedDescription)")
             }
         }
+        
+        notificationToken = realm.observe { [weak self] (notification, realm) in
+            self?.updateUI(specificDate: specificDate)
+        }
 
       
     }
+    
+    func updateUI(specificDate: String) {
+          // update UI based on the latest data in Realm
+          let entry = realm.objects(Entry.self).filter("dayCurrent = %@", specificDate).first
+          journalEntry.text = entry?.journal
+          photoCaption.text = entry?.photoCaption
+          
+          // update image, assuming entry is an instance of Entry object
+          let imagePath = specificDate + ".jpg"
+          let fileManager = FileManager.default
+
+          if let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first {
+              let fileURL = documentDirectory.appendingPathComponent(imagePath)
+              do {
+                  let imageData = try Data(contentsOf: fileURL)
+                  let image = UIImage(data: imageData)
+                  imageJournal.image = image
+                  print("uploaded image")
+              } catch {
+                  print("Error loading image: \(error.localizedDescription)")
+              }
+          }
+      }
+
+      deinit {
+          notificationToken?.invalidate()
+      }
+
 }
 
