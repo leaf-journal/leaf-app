313669e656fd917e0b265c670637f774c2a167fb
Linh <linhvu.138@utexas.edu>
2023-04-16 04:38:21 -0500

    added custom cell back

---
+++ leaf-journal/CustomTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTableViewCell.swift
+//  leaf-journal
+//
+//  Created by L V on 4/16/23.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var iconImageView : UIImageView!
+    @IBOutlet weak var lable : UILabel!
+
+}
